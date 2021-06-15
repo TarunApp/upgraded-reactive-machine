532af58522ef338be80d259a6a8d6c9e0a87f424
Hosely5 <tappannagari@gmail.com>
2021-06-15 00:29:00 -0500

    MDX Testing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
-const Card = ({languages}) => {
+const Card = (props) => {
 	return (
 		<div className="card">
-			<h1>Card Title</h1>
+			<h1>{props.name}</h1>
 			<div className="card-flex">
 				<div className="card-one">
 					<h2>Project Details</h2>
-					<p>{languages}</p>
+					<p>{props.languages}</p>
 				</div>
 				<div className="card-content">
 					<a href="https://github.com">GitHub</a>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,28 +1,29 @@
 import {useState, useEffect} from 'react'
 import Card from "./Card";
+import "../App.css"
 
 const Projects = (props) => {
 
-	const [input, SetInput] = useState(" ")
+	const [input, SetInput] = useState("") //Do not make input with spaces
 
 	let posts = [{name: 'test', languages: 'JS'}, {name: 'asdjk', languages: 'Python'}]
 
 	useEffect(() => {
-		// 
+		fetch('https://api.github.com/users/TarunApp/repos')
+		.then(res => res.json())
+		.then(data => console.log(data))
 	},[])
 
 	let onInput = (e) => {
 		SetInput(e.target.value)
 	}
 
+				// {input.length != "" ? posts.filter((item) => item.name.includes(input)).map(item => <li key={item.languages}>{item.name}</li>) : posts.map((item) => <li key={item.languages}>{item.name}</li>)}
 	return (
 		<div className="projects">
-			<input onChange={onInput} value={input} type="text"/>
-			<p>{input}</p>
-			<ul>
-			<h1>CONDITIONAL RENDERING ROCKS!!!!!!</h1>
-				{input.length > 0 ? posts.filter((item) => item.name.includes(input)).map(item => <li>{item.name}</li>) : posts.map((item) => <li>{item.name}</li>)}
-			</ul>
+			<label htmlFor="search"><h2>Search: {input}</h2></label>
+			<input name="search" onChange={onInput} value={input} type="text"/>
+			{input == "" ? posts.map(item => <Card name={item.name} languages={item.languages}/>) : posts.filter((item) => item.name.includes(input)).map(item => <Card name={item.name} languages={item.languages}/>)}
 		</div>
 	);
 };
